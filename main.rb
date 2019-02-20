271e72bb5714bfe98cb472658e4dc8b00a0cc553
Du Nguyen <nguyend33@gmail.com>
2019-02-19 20:22:09 -0700

    add git command code

---
+++ main.rb
@@ -0,0 +1,22 @@
+def puts_git(cmd)
+  puts `git #{cmd} -h`
+  menu
+end
+
+def menu
+  puts '1: Enter git command'
+  puts '2: Exit'
+  choice = gets.to_i
+  case choice
+  when 1
+    puts 'Enter command'
+    puts_git(gets.strip)
+  when 2 
+    exit
+  else
+    puts 'Invalid choice'
+    menu
+  end
+end
+
+menu
